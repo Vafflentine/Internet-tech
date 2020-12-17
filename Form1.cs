1c44b18c18d0f027fedd59c0031ec4a015b2256e
Vafflentine <55801215+Vafflentine@users.noreply.github.com>
2020-12-17 16:28:49 +0200

    C# gmail api

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using AE.Net.Mail;
+namespace Itech
+{
+    public partial class Form1 : Form
+    {
+        static Form1 f;
+        static ImapClient IC;
+        MailMessage email = null;
+        public Form1()
+        {
+            InitializeComponent();
+            f = this;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IC = new ImapClient("imap.gmail.com", textBox1.Text, textBox2.Text, AuthMethods.Login, 993, true);
+                IC.SelectMailbox("INBOX");
+                for (int i = 1; i < 10; i++)
+                {
+                    email = IC.GetMessage(IC.GetMessageCount() - i);
+                    textBox3.AppendText("From: " + email.From + Environment.NewLine);
+                    textBox3.AppendText("Subject: " + email.Subject + Environment.NewLine);
+                    textBox3.AppendText("Body: " + email.Body + Environment.NewLine);
+                    textBox3.AppendText("***********************************************************************************" + Environment.NewLine);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Inncorect email or password");
+            }
+        }
+    }
+}   
